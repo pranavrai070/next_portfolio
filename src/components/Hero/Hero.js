0107a94981a3f08defb86531835588117d734758
pranavrai070 <pranavrai070@gmail.com>
2024-04-18 15:00:04 +0530

    resume, hero updated

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -11,12 +11,12 @@ const Hero = (props) => (
       <LeftSection>
         <SectionTitle main center>
           Welcome To <br />
-          My Personal Portfolio
+          Pranav's Portfolio
         </SectionTitle>
         <SectionText>
         The purpose of this portfolio to show all my skills and works together in a simple and efficient manner.
         </SectionText>
-        <Link href="https://drive.google.com/file/d/1D2eNKoEAPvYWntQKy0acxcXm8L4lGQXs/view?usp=sharing" legacyBehavior>
+        <Link href="https://drive.google.com/file/d/1jOWXj-3Xoj2J1DXMZ4EcGSGKd4onteJn/view?usp=sharing" legacyBehavior>
         <a target='blank'><Button onClick={props.handleClick}>Checkout Resume</Button></a>
         </Link>
       
